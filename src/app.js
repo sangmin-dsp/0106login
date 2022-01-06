b616e6665d4d7106ffc3028109f9f145ce88e93f
SangminSeok <mannone@rangeduck.com>
2022-01-06 03:36:23 +0100

    소스코드 폴더로 분류

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ const app = express();
 
 
 //라우팅 
-const home = require('./routes/home'); // ( ) -> index.js파일을 읽음
+const home = require('../routes/home'); // ( ) -> index.js파일을 읽음
 
 //앱 세팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
+app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드...
 
 module.exports = app;
--- src/bin/www.js
+++ src/bin/www.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const PORT = 3000;
-const app = require("../app");
+const app = require("../src/app");
 
 app.listen(PORT, () => {
   console.log("Server is connected...")
